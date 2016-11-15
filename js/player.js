bdbeaf684041e09a6421393f94fa6063759f4cbb
cshanes <cshanes@umich.edu>
2016-11-15 14:29:50 -0500

    Added initial radial bar chart, fixed some of the drag n drop stuff. Reverted to d3 v3

--- js/player.js
+++ js/player.js
@@ -1,13 +1,12 @@
 var width = 3000;
-var height = 3000;
-colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+var height = 300;
+colorScale = d3.scale.ordinal(d3.schemeCategory10);
 
+var selectedPlayerList = [];
 
 function init() {
-    area = d3.select('#container').append('svg')
-        .attr("width", width)
-        .attr("height", height);
     readIn();
+    drawRadialBarChart();
 }
 
 function getTranslation(transform) {
@@ -29,7 +28,7 @@ function getTranslation(transform) {
 }
 
 function dragstarted(d) {
-    d3.select(this).raise().classed("active", true);
+    // d3.select(this).raise().classed("active", true);
 }
 
 function dragged(d) {
@@ -44,32 +43,42 @@ function dragged(d) {
 }
 
 function dragended(d) {
+    currentX = getTranslation(d3.select(this).attr("transform"))[0]
+    getFinal(d, currentX)
     d3.select(this).classed("active", false);
 }
 
-function getFinal(d) {
-    //if box within rectangle 2, then return box id
+function getFinal(d, currentX) {
+
+    if (currentX >= 200) {
+        selectedPlayerList.push(d.key);
+        console.log(selectedPlayerList)
+    }
 }
+
 function readIn() {
     d3.csv('data/playerData.csv', data);
 }
 function data(rawdata) {
     playerData = d3.nest()
-        .key(function (d, i) {
-            return (d.playername);
+        .key(function (d) {
+            return (d.abrev_name);
         })
         .entries(rawdata);
 
-    var drag = d3.drag()
-        .on("start", dragstarted)
+    var drag = d3.behavior.drag()
+        .on("dragstart", dragstarted)
         .on("drag", dragged)
-        .on("end", dragended);
+        .on("dragend", dragended);
 
-    container = area.selectAll("g")
-        .enter().append("g");
+    var area = d3.select('#player_select').append('svg')
+        .attr("width", width)
+        .attr("height", height);
 
-    var rectWidth = 60;
-    var rectPadding = 40;
+    var container = area.append("g");
+
+    var rectWidth = 40;
+    var rectPadding = 20;
 
     container.append("rect")
         .attr("x", rectWidth)
@@ -94,7 +103,7 @@ function data(rawdata) {
         .attr("class", "bar")
         .attr("transform", function (d, i) {
             xVal = i * rectWidth + rectPadding;
-            yVal = 30;
+            yVal = 0;
             return "translate(" + [xVal, yVal] + ")"
         })
         .call(drag);
@@ -114,4 +123,117 @@ function data(rawdata) {
       .attr("x", function(d,i){return (rectPadding + (rectWidth * i))})
       .attr("y", 80)
       .attr("font-family", "sans-serif");
+}
+
+function drawRadialBarChart() {
+    var width = 960,
+        height = 500,
+        barHeight = height / 2 - 40;
+
+    var formatNumber = d3.format("s");
+
+    var color = d3.scale.ordinal()
+        .range(["#8dd3c7","#ffffb3","#bebada","#fb8072","#80b1d3","#fdb462","#b3de69","#fccde5","#d9d9d9","#bc80bd","#ccebc5","#ffed6f"]);
+
+    var svg = d3.select('#chart').append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+        .attr("transform", "translate(" + width/2 + "," + height/2 + ")");
+
+    d3.csv("data/playerData.csv", function(error, data) {
+
+        data.map(function(d) {
+            return d['value'] = +d['value'];
+        })
+
+        data.sort(function(a,b) { return b.value - a.value; });
+
+        var extent = d3.extent(data, function(d) { return d.value; });
+        var barScale = d3.scale.linear()
+            .domain(extent)
+            .range([0, barHeight]);
+
+        var keys = data.map(function(d,i) { return d.abrev_name; });
+        var numBars = keys.length;
+
+        var x = d3.scale.linear()
+            .domain(extent)
+            .range([0, -barHeight]);
+
+        var xAxis = d3.svg.axis()
+            .scale(x).orient("left")
+            .ticks(3)
+            .tickFormat(formatNumber);
+
+        var circles = svg.selectAll("circle")
+            .data(x.ticks(3))
+            .enter().append("circle")
+            .attr("r", function(d) {return barScale(d);})
+            .style("fill", "none")
+            .style("stroke", "black")
+            .style("stroke-dasharray", "2,2")
+            .style("stroke-width",".5px");
+
+        var arc = d3.svg.arc()
+            .startAngle(function(d,i) { return (i * 2 * Math.PI) / numBars; })
+            .endAngle(function(d,i) { return ((i + 1) * 2 * Math.PI) / numBars; })
+            .innerRadius(0);
+
+        var segments = svg.selectAll("path")
+            .data(data)
+            .enter().append("path")
+            .each(function(d) { d.outerRadius = 0; })
+            .style("fill", function (d) { return color(d.name); })
+            .attr("d", arc);
+
+        segments.transition().ease("elastic").duration(1000).delay(function(d,i) {return (25-i)*100;})
+            .attrTween("d", function(d,index) {
+                var i = d3.interpolate(d.outerRadius, barScale(+d.value));
+                return function(t) { d.outerRadius = i(t); return arc(d,index); };
+            });
+
+        // svg.append("circle")
+        //     .attr("r", barHeight)
+        //     .classed("outer", true)
+        //     .style("fill", "none")
+        //     .style("stroke", "black")
+        //     .style("stroke-width","1.5px");
+
+        // var lines = svg.selectAll("line")
+        //     .data(keys)
+        //     .enter().append("line")
+        //     .attr("y2", -barHeight - 20)
+        //     .style("stroke", "black")
+        //     .style("stroke-width",".5px")
+        //     .attr("transform", function(d, i) { return "rotate(" + (i * 360 / numBars) + ")"; });
+
+        svg.append("g")
+            .attr("class", "x axis")
+            .call(xAxis);
+
+        // Labels
+        var labelRadius = barHeight * 1.025;
+
+        var labels = svg.append("g")
+            .classed("labels", true);
+
+        labels.append("def")
+            .append("path")
+            .attr("id", "label-path")
+            .attr("d", "m0 " + -labelRadius + " a" + labelRadius + " " + labelRadius + " 0 1,1 -0.01 0");
+
+        labels.selectAll("text")
+            .data(keys)
+            .enter().append("text")
+            .style("text-anchor", "middle")
+            .style("font-weight","bold")
+            .style("fill", function(d, i) {return "#3e3e3e";})
+            .append("textPath")
+            .attr("xlink:href", "#label-path")
+            .attr("startOffset", function(d, i) {return i * 100 / numBars + 50 / numBars + '%';})
+            .text(function(d) {
+                return d.toUpperCase();
+            });
+    });
 }
